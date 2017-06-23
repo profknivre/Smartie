afca25c249c7995fb791b64e4eeb9a4c6c0f5e6e
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2017-06-23 11:08:50 +0200

    refactoring step2

--- dmp.py
+++ dmp.py
@@ -1,39 +1,14 @@
+from temp import linreg
 from collections import deque
-import pickle
+import shelve
 
-fname = '/tmp/histogram'
+with shelve.open('/tmp/shelve') as db:
+    if 'hum_hist' not in db:
+        raise Exception('not found')
+    else:
+        dq = db['hum_hist']
 
-def linreg(X, Y):
-    """
-    return a,b in solution to y = ax + b such that root mean square distance between trend line and original points is minimized
-    """
-    N = len(X)
-    Sx = Sy = Sxx = Syy = Sxy = 0.0
-    for x, y in zip(X, Y):
-        Sx = Sx + x
-        Sy = Sy + y
-        Sxx = Sxx + x*x
-        Syy = Syy + y*y
-        Sxy = Sxy + x*y
-    det = Sxx * N - Sx * Sx
-    return (Sxy * N - Sy * Sx)/det, (Sxx * Sy - Sx * Sxy)/det
-
-
-try:
-    with open(fname,'rb') as f:
-        humtab = pickle.loads(f.read())
-
-except:
-    humtab = deque(maxlen=10)
- 
-    
-humlst = list(humtab)
-a,b = linreg(range(len(humlst)),humlst)
-
-print(humlst)
-print(a)
-
-
-
-    
+    print(dq)
+    a, b = linreg(range(len(dq)), dq)
+    print(a)
     
--- temp.py
+++ temp.py
@@ -5,34 +5,54 @@ import shelve
 import subprocess
 from collections import deque
 
-import Adafruit_DHT
-import statsd
+try:
+    import Adafruit_DHT
+    import statsd
+except ModuleNotFoundError:
+    pass
+
 from apixu import getdata
 
-c = statsd.StatsClient('5.8.1.1', 8125)
+
+# TODO make this proper
+# ---decorator hack
+try:
+    # in my network 5.8.0.0/16 is not routed outside!!!
+    stats = statsd.StatsClient('5.8.1.1', 8125)
+except NameError:
+    class TimerMock:
+        @staticmethod
+        def timer(args):
+            def a(arg):
+                pass
+            return a
+    stats = TimerMock
+# ---hack
+
+# TODO: add some testing!!
+
+
+def linreg(x, y):
+    """
+    return a,b in solution to y = ax + b such that root mean square distance between trend line
+    and original points is minimized
+    """
+    n = len(x)
+    sx = sy = sxx = syy = sxy = 0.0
+    for x_, y_ in zip(x, y):
+        sx = sx + x_
+        sy = sy + y_
+        sxx = sxx + x_ * x_
+        syy = syy + y_ * y_
+        sxy = sxy + x_ * y_
+    det = sxx * n - sx * sx
+    try:
+        return (sxy * n - sy * sx) / det, (sxx * sy - sx * sxy) / det
+    except ZeroDivisionError:
+        return 0
 
 
 def get_slope(current_val, update=True):
-
-    def linreg(x, y):
-        """
-        return a,b in solution to y = ax + b such that root mean square distance between trend line
-        and original points is minimized
-        """
-        n = len(x)
-        sx = sy = sxx = syy = sxy = 0.0
-        for x_, y_ in zip(x, y):
-            sx = sx + x_
-            sy = sy + y_
-            sxx = sxx + x_ * x_
-            syy = syy + y_ * y_
-            sxy = sxy + x_ * y_
-        det = sxx * n - sx * sx
-        try:
-            return (sxy * n - sy * sx) / det, (sxx * sy - sx * sxy) / det
-        except ZeroDivisionError:
-            return 0
-
     with shelve.open('/tmp/shelve') as db:
         if 'hum_hist' not in db:
             dq = deque(maxlen=10)
@@ -50,6 +70,7 @@ def get_slope(current_val, update=True):
         return a
 
 
+@stats.timer('malina0.measurments_time.ds18read')
 def readds18():
     with open('/sys/bus/w1/devices/28-0115915119ff/w1_slave', 'r') as f:
         f.readline()
@@ -58,68 +79,90 @@ def readds18():
         return z
 
 
+@stats.timer('malina0.measurments_time.coretemp')
 def readcroetemp():
     return float(
         subprocess.check_output(['/opt/vc/bin/vcgencmd', 'measure_temp']).decode('utf8').strip()[5:].replace("'C", ''))
 
 
-def fancontrol(humidity, humi_slope):
+@stats.timer('malina0.measurments_time.dhtread')
+def readdht():
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 19)
+    return humidity, temperature
+
+
+@stats.timer('malina0.measurments_time.apixu')
+def readapixu():
+    apixu = getdata()
+    temperature = apixu['current']['temp_c']
+    humidity = apixu['current']['humidity']
+
+    return temperature, humidity
+
+
+def fancontrol(humidity, slope):
+    pin =13
+    prefix = '/sys/class/gpio/'
+    gpio = 'gpio{}'.format(pin)
+    value_path = '{}{}/value'.format(prefix,gpio)
+    direction_path = '{}{}/direction'.format(prefix,gpio)
+
     if not os.path.exists('/tmp/bypass'):
-        if not os.path.exists('/sys/class/gpio/gpio13/value'):
+        if not os.path.exists(value_path):
             with open('/sys/class/gpio/export', 'w') as file:
                 file.write('13\n')
 
-            with open('/sys/class/gpio/gpio13/direction', 'w') as file:
+            with open(direction_path, 'w') as file:
                 file.write('out\n')
 
-            with open('/sys/class/gpio/gpio13/value', 'w') as file:
+            with open(value_path, 'w') as file:
                 file.write('0\n')
 
         if humidity >= 80:
-            with open('/sys/class/gpio/gpio13/value', 'w') as file:
+            with open(value_path, 'w') as file:
                 file.write('1\n')
 
         # if humidity <=65:
-        if humidity < 75 and abs(humi_slope) < 0.25:
-            with open('/sys/class/gpio/gpio13/value', 'w') as file:
+        if humidity < 75 and abs(slope) < 0.25:
+            with open(value_path, 'w') as file:
                 file.write('0\n')
 
-        if not os.path.exists('/sys/class/gpio/gpio13/value'):
+        if not os.path.exists(value_path):
             raise Exception('no gpio')
 
-    with open('/sys/class/gpio/gpio13/value') as file:
+    with open(value_path) as file:
         val = int(file.read().strip())
-        c.gauge('mieszkanie.lazienka.wentylator', val)
+        stats.gauge('mieszkanie.lazienka.wentylator', val)
 
 
-with c.timer('malina0.measurments_time.total'):
-    with c.timer('malina0.measurments_time.ds18read'):
+def main():
+    with stats.timer('malina0.measurments_time.total'):
+
         ds18temp = readds18()
-        c.gauge('mieszkanie.temp1', ds18temp)
+        stats.gauge('mieszkanie.temp1', ds18temp)
 
-    with c.timer('malina0.measurments_time.coretemp'):
-        tmp = readcroetemp()
-        c.gauge('malina0.core_temp', tmp)
+        coretemp = readcroetemp()
+        stats.gauge('malina0.core_temp', coretemp)
 
-    with c.timer('malina0.measurments_time.dhtread'):
-        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 19)
+        humidity, temperature = readdht()
 
-    c.gauge('mieszkanie.lazienka.temp', temperature)
-    c.gauge('mieszkanie.lazienka.humidity', humidity)
+        stats.gauge('mieszkanie.lazienka.temp', temperature)
+        stats.gauge('mieszkanie.lazienka.humidity', humidity)
 
-    humi_slope = get_slope(humidity)
+        slope = get_slope(humidity)
 
-    c.gauge('mieszkanie.lazienka.humidity_slope', humi_slope)
+        stats.gauge('mieszkanie.lazienka.humidity_slope', slope)
 
-    # TODO pretty print this shite
-    print("temp1: {:2.2f} temp lazienka: {:2.2f} humi lazienka: {:2.2f}".format(ds18temp, temperature, humidity))
+        # TODO pretty print this shite
+        print("temp1: {:2.2f} temp lazienka: {:2.2f} humi lazienka: {:2.2f}".format(ds18temp, temperature, humidity))
 
-    with c.timer('malina0.measurments_time.apixu'):
-        apixu = getdata()
-    temp = apixu['current']['temp_c']
-    ahum = apixu['current']['humidity']
+        apixtemp, apixhum = readapixu()
 
-    c.gauge('mieszkanie.temp_zew', temp)
-    c.gauge('mieszkanie.humi_zew', ahum)
+        stats.gauge('mieszkanie.temp_zew', apixtemp)
+        stats.gauge('mieszkanie.humi_zew', apixhum)
 
-    fancontrol(humidity, humi_slope)
+        fancontrol(humidity, slope)
+
+
+if __name__ == '__main__':
+    main()
