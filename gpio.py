a8dfd704405ae0db4d0761aa29aa0a48ae75ff7b
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2017-07-19 22:44:29 +0200

    added cmdline support for gpio

--- gpio.py
+++ gpio.py
@@ -1,11 +1,12 @@
-from abc import abstractmethod
 import os
+from abc import abstractmethod
+
 
 class GPIOBase:
     DDR_OUTPUT = 1
     DDR_INPUT = 0
 
-    DIRs = frozenset((DDR_OUTPUT,DDR_INPUT))
+    DIRs = frozenset((DDR_OUTPUT, DDR_INPUT))
 
     @abstractmethod
     def setDDR(self, direction):
@@ -23,27 +24,28 @@ class GPIOBase:
     def getDDR(self):
         pass
 
+
 class GPIO(GPIOBase):
-    def __init__(self,gp_impl=GPIOBase()):
-        self._gp_impl=gp_impl
+    def __init__(self, gp_impl=GPIOBase()):
+        self._gp_impl = gp_impl
 
     def setOutput(self, value):
-        gp=self._gp_impl
+        gp = self._gp_impl
         if self.getDDR() == GPIOBase.DDR_OUTPUT:
             return gp.setOutput(value)
         else:
             raise Exception("GPIO: trying to force value on input pin")
 
     def getInput(self):
-        gp=self._gp_impl
+        gp = self._gp_impl
 
-        #if self.getDDR(pin) == GPIOBase.DDR_INPUT:
+        # if self.getDDR(pin) == GPIOBase.DDR_INPUT:
         return gp.getInput()
-        #else:
+        # else:
         #    raise Exception("GPIO: trying to read value from output pin")
 
     def setDDR(self, direction):
-        gp=self._gp_impl
+        gp = self._gp_impl
 
         if direction in GPIOBase.DIRs:
             return gp.setDDR(direction)
@@ -51,12 +53,12 @@ class GPIO(GPIOBase):
             raise Exception("GPIO: wrong direction: {0:d}".format(direction))
 
     def getDDR(self):
-        gp=self._gp_impl
+        gp = self._gp_impl
         return gp.getDDR()
 
 
 class SysfsGPIO(GPIOBase):
-    def __init__(self,pinnumber=13) :
+    def __init__(self, pinnumber=13):
         super().__init__()
 
         self._pinnumber = pinnumber
@@ -65,16 +67,15 @@ class SysfsGPIO(GPIOBase):
         self.value_path = '{}{}/value'.format(self.prefix, self.gpio)
         self.direction_path = '{}{}/direction'.format(self.prefix, self.gpio)
 
-        self.dirmap = {self.DDR_INPUT : 'in',
-                       self.DDR_OUTPUT : 'out'}
+        self.dirmap = {self.DDR_INPUT: 'in',
+                       self.DDR_OUTPUT: 'out'}
 
-        self.rdirmap = {'in' : self.DDR_INPUT,
+        self.rdirmap = {'in': self.DDR_INPUT,
                         'out': self.DDR_OUTPUT}
 
         if not os.path.exists(self.value_path):
             with open(self.prefix + 'export', 'w') as file:
                 file.write('{}\n'.format(self._pinnumber))
-
 
     def setOutput(self, value):
         with open(self.value_path, 'w') as file:
@@ -82,15 +83,42 @@ class SysfsGPIO(GPIOBase):
 
     def setDDR(self, direction):
         with open(self.direction_path, 'w') as file:
-            file.write('{}\n'.format(self.dirmap.get(direction,self.DDR_INPUT)))
+            file.write('{}\n'.format(self.dirmap.get(direction, self.DDR_INPUT)))
 
     def getDDR(self):
         with open(self.direction_path, 'r') as file:
             val = file.read().strip()
-            return self.rdirmap.get(val,self.DDR_INPUT)
+            return self.rdirmap.get(val, self.DDR_INPUT)
 
     def getInput(self):
         with open(self.value_path, 'r') as file:
             val = int(file.read().strip())
             return val
 
+
+def Main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='fiddle with gpio')
+    parser.add_argument('pin', metavar='pinnum', type=int, nargs='?', help='gpio pin number', default=13)
+    parser.add_argument('val', metavar='val', type=int, nargs='?', help='value', default=None, choices=(0, 1))
+    parser.add_argument('--ddr', metavar='direction', type=int, nargs=1, help='data direction register', default=None,
+                        choices=(0, 1))
+
+    args = parser.parse_args()
+    # print(args)
+
+    gp = SysfsGPIO(pinnumber=args.pin)
+
+    if args.ddr != None:
+        gp.setDDR(args.ddr)
+
+    if (args.val == None):
+        print(gp.getInput())
+        return
+    else:
+        gp.setOutput(args.val)
+
+
+if __name__ == '__main__':
+    Main()
