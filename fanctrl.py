1defbb678dd38b9531d59ddae99f49564aaed179
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2017-10-18 00:02:34 +0200

    automatic condition loading

--- fanctrl.py
+++ fanctrl.py
@@ -1,5 +1,3 @@
-import logging
-
 from conditions import *
 from fan import TimedFan
 from measurements import Measurements
@@ -7,19 +5,26 @@ from measurements import Measurements
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+modules = dir()
 
 class FanController:
     def __init__(self, fan: TimedFan, measurements: Measurements):
+        global modules
         self.fan = fan
         self.measurements = measurements
 
-        self.startconds = [HighHumidityAndHighSlopeCondtion(self.fan, self.measurements),
-                           ForceStartCondition(self.fan, self.measurements)]
+        conditions = filter(lambda x: 'Condition' in x, modules)
+        self.startconds = []
+        self.stopconds = []
 
-        self.stopconds = [LowHumiditySmallSlopeCondition(self.fan, self.measurements),
-                          LongRunningTimeCondition(self.fan, self.measurements),
-                          ForceStopCondition(self.fan, self.measurements)]
-
+        for cond in conditions:
+            cls = eval(cond)
+            if issubclass(cls, FanStartCond):
+                self.startconds.append(cls(self.fan, self.measurements))
+            elif issubclass(cls, FanStopCond):
+                self.stopconds.append(cls(self.fan, self.measurements))
+            else:
+                log.error('{:s} is neither FanStartCond nor FanStopCond'.format(cond))
 
     def do_stuff(self):
         if not os.path.exists('/tmp/bypass'):
