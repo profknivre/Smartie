ddc339b21ffeb3a161a20451f1671e708e066371
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2017-10-21 22:01:40 +0200

    make dht configurable(poorly)

--- measurements/dht.py
+++ measurements/dht.py
@@ -13,13 +13,14 @@ class Dht(BaseMeasurement):
     def __init__(self, **kwargs):
         if 'timing_caption' not in kwargs:
             kwargs['timing_caption'] = 'malina0.measurments_time.dhtread'
+        self.dht_read_params = kwargs.get('dht_read_params', 'Adafruit_DHT.DHT22, 19')  # TODO: ugly as fuck
         super().__init__(**kwargs)
 
     def read(self):
         humidity, temperature = 0, 0
         try:
             import Adafruit_DHT
-            humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 19)
+            humidity, temperature = Adafruit_DHT.read_retry(*eval(self.dht_read_params))  # TODO: ugly as fuck
         except Exception as e:
             log.error(e)
 
