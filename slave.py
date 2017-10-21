7480d47f90b3c5eb107d1e114385baa5652b5f3e
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2017-10-22 01:05:22 +0200

    super simple command dispatcher basic resource monitoring

--- slave.py
+++ slave.py
@@ -1,5 +1,72 @@
-import rpyc
+import logging
+import threading
+from abc import ABC, abstractmethod
+from contextlib import suppress
 
+import rpyc
+import statsd
+
+logging.basicConfig(level=logging.DEBUG)
+
+from time import sleep
+
+# in my network 5.8.0.0/16 is not routed outside!!!
+stats = statsd.StatsClient('5.8.1.1', 8125)
+
+monitoring = True
+server = None
+
+
+class BaseCommand(ABC):
+    def __init__(self, **kwargs):
+        self.service = kwargs.get('service')
+        super().__init__()
+
+    @abstractmethod
+    def exec(self, **kwargs):
+        pass
+
+    @staticmethod
+    def build(**kwargs):
+        ret = {}
+        for cls in BaseCommand.__subclasses__():
+            ret[cls.NAME] = cls(**kwargs)
+        return ret
+
+
+class ReadDs18(BaseCommand):
+    NAME = 'read_ds18'
+
+    def exec(self, **kwargs):
+        from measurements.ds18 import Ds18
+        ds = Ds18(**kwargs)  # 28-0115916115ff
+        retval = ds.read()
+        return retval
+
+
+class ReadDHT(BaseCommand):
+    NAME = 'read_dht'
+
+    def exec(self, **kwargs):
+        from measurements.dht import Dht
+        dht = Dht(dht_read_params='Adafruit_DHT.DHT22, 4')
+        retval = dht.read()
+        return retval
+
+
+class Disconnect(BaseCommand):
+    NAME = 'disconnect'
+
+    def exec(self, **kwargs):
+        self.service._conn.close()
+
+
+class KillServer(BaseCommand):
+    NAME = 'kill'
+
+    def exec(self, **kwargs):
+        if server is not None:
+            server.close()
 
 class SmartieSlave(rpyc.Service):
     ALIASES = ['SmartieSlave']
@@ -14,22 +81,42 @@ class SmartieSlave(rpyc.Service):
         # (to finalize the service, if needed)
         pass
 
-    def exposed_get_ds18_temp(self, sensor_id='28-0115916115ff'):
-        from measurements.ds18 import Ds18
-        ds = Ds18(sensor_id=sensor_id)  # 28-0115916115ff
+    def exposed_tst(self):
+        Disconnect(service=self).exec()
 
-        retval = ds.read()
-        return retval
+    def exposed_cmd(self, name, **kwargs):
+        cmds = BaseCommand.build(service=self)
 
-    def exposed_get_dht_data(self):
-        from measurements.dht import Dht
-        dht = Dht(dht_read_params='Adafruit_DHT.DHT22, 4')
-        retval = dht.read()
-        return retval
+        if name in cmds:
+            return cmds.get(name).exec(**kwargs)
 
+
+def worker():
+    import psutil
+    from platform import node
+
+    hostname = node().replace('.lan', '')
+
+    while True:
+        pvm = psutil.virtual_memory()
+        nfo = dict(filter(lambda x: x[0] in ('used', 'free'), zip(pvm._asdict(), pvm)))
+
+        for k in nfo.keys():
+            caption = '{}.memory.{}'.format(hostname, k)
+            if monitoring:
+                stats.gauge(caption, nfo.get(k))
+            print(caption, nfo.get(k))
+        sleep(60)
 
 if __name__ == "__main__":
-    from rpyc.utils.server import ThreadedServer
+    from rpyc.utils.server import ThreadedServer as RpycServer
 
-    t = ThreadedServer(SmartieSlave, port=18861, auto_register=True)
-    t.start()
+    registrar = rpyc.utils.registry.TCPRegistryClient('44.0.0.171')
+    t = RpycServer(SmartieSlave, port=18861, auto_register=True, registrar=registrar)
+    t2 = threading.Thread(target=worker, name='ResourceMonitor', daemon=True)
+
+    server = t
+
+    with suppress(KeyboardInterrupt):
+        t2.start()
+        t.start()
