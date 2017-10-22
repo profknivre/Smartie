d3b38739e7bee7dc352f97fc11204b1c0aa6bd40
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2017-10-22 18:46:37 +0200

    import optimisation fuckup

--- measurements/factory.py
+++ measurements/factory.py
@@ -1,3 +1,13 @@
+from .coretemp import CoreTemp
+from .dht import Dht
+from .ds18 import Ds18
+from .online_weather import OnlineWeather
+
+
+def __dupa():
+    a = [CoreTemp, Dht, Ds18, OnlineWeather]
+    return a
+
 def get_measurements():
     from .base_measurement import BaseMeasurement
 
