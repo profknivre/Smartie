9487a587812c3836e31b47a53c267947d5fab2fd
Grzegorz Zebrowski <grzegorz.zebrowski@thrust.pl>
2018-06-13 19:41:23 +0200

    XMLRPC slave mod

--- measurements/mesurement_internals.py
+++ measurements/mesurement_internals.py
@@ -52,21 +52,25 @@ class MeasurementsInternals2():
 
         try:
             with timeout(5):
-                import rpyc
-    
-                registrar = rpyc.utils.registry.TCPRegistryClient('5.8.0.8')
-                ret = rpyc.utils.factory.discover('SmartieSlave', registrar=registrar)
-    
-                conn = rpyc.connect(*ret[0])
+                # import rpyc
+                # registrar = rpyc.utils.registry.TCPRegistryClient('5.8.0.8')
+                # ret = rpyc.utils.factory.discover('SmartieSlave', registrar=registrar)
+                # conn = rpyc.connect(*ret[0])
+
+                from disco import DiscoProxy2
+                dp2=DiscoProxy2()
+
+                conn = dp2.SmartieSlave
+
                 self.radiator_temperature = RemoteAdapter(Ds18, gauge_caption='mieszkanie.kaloryfer.temp',
                                                           sensor_id='28-0115916115ff', conn=conn)
-    
+
                 remote_dht = RemoteAdapter(Dht, dht_read_params='Adafruit_DHT.DHT22, 4', conn=conn,
                                            timing_caption='malina2.measurments_time.dht22read')
-    
+
                 self.bedroom_temperature = UberAdapter(remote_dht, 1, gauge_caption='mieszkanie.test22.temp')
                 self.bedroom_humidity = UberAdapter(remote_dht, 0, gauge_caption='mieszkanie.test22.humidity')
-    
+
                 self.malina2_core_temperature = RemoteAdapter(CoreTemp, gauge_caption='malina2.core_temp',
                                                               timing_caption='malina2.measurments_time.coretemp',
                                                               conn=conn)
